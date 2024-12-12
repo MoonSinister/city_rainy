928d0cbe67b14948326901d0518c17290b8ed2b5
moonst <245823974@qq.com>
2024-12-12 17:08:27 +0800

    修正readroute，获得站点json数据

--- engine/BaseModule/readmap.py
+++ engine/BaseModule/readmap.py
@@ -4,7 +4,7 @@ from shapely.geometry import Point
 import matplotlib.pyplot as plt
 # 创建一个空的GeoDataFrame
 
-gdf = gpd.read_file('天津_线路.shp',encoding = 'utf-8')
+gdf = gpd.read_file('天津_站点.shp',encoding = 'utf-8')
 
 
 
@@ -16,4 +16,4 @@ gdf.plot(ax=ax, color='blue', edgecolor='black', linewidth=0.5)
 plt.title('Custom Map Style')
 plt.show()
 """
-gdf.to_file("route.geojson", driver='GeoJSON') 
+gdf.to_file("site.geojson", driver='GeoJSON')
